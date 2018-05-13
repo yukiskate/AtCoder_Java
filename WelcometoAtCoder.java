2116854e8b5fd0f9a3a16193f1a890f6804ba945
uranaka <39235614+yukiskate@users.noreply.github.com>
2018-05-13 21:42:29 +0900

    A - はじめてのあっとこーだー（Welcome to AtCoder）

---
+++ WelcometoAtCoder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WelcometoAtCoder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//整数の入力
+		int iSuji1 = sc.nextInt();
+		//スペース区切りの整数の入力
+		int iSuji2 = sc.nextInt();
+		int iSuji3 = sc.nextInt();
+
+		//文字列の入力
+		String strMoji = sc.next();
+		//出力
+		System.out.println((iSuji1+iSuji2+iSuji3) + " " + strMoji);
+	}
+}
